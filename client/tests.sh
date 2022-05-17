c81eb0265e6c13f14e9294fbc72cd92ef26d24d8
mike <mikelka150@gmail.com>
2022-05-17 05:05:56 +0300

    added all needed functions

---
+++ client/tests.sh
@@ -0,0 +1,6 @@
+node createAsset.js 12435678 Vladimir Ustugov Moscow Kozlova-29 +7134234 Maried
+node readAssetData.js 12435678
+node updateAsset.js 12435678 Vladimir Ustugov Moscow Kozlova-29 +7134234 Unmaried
+node readAssetChangesHistory.js 12435678
+node readAllAssets.js
+node deleteAsset.js 12435678
